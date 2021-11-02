fa6bb3b78d83b9ca8d312ac1f1dd30282a38f63b
Brandon <brrrandonhuang@gmail.com>
2021-11-02 13:44:39 -0700

    new

--- ZonalWindRunner.py
+++ ZonalWindRunner.py
@@ -16,34 +16,34 @@ print(latitude)
 
 #Generate an array of latitudes (pixels) and best velocities (m/s). 
 #for lat in latitude:
-# lats = [-23.05,-23.00,-22.95,-22.90,-22.85,-22.80]
-# v_corr = np.zeros_like(latitude)*np.nan
-# obj_list = []
-# for lat in lats:
-#     try:
-#         obj_list.append(v_maxcorr.remote(lat, path2data=path2data, plotting=False, vstep=361))
-#     except:
-#         print("error at ", lat)
+lats = [-23.05,-23.00,-22.95,-22.90,-22.85,-22.80]
+v_corr = np.zeros_like(latitude)*np.nan
+obj_list = []
+for lat in lats:
+    try:
+        obj_list.append(v_maxcorr.remote(lat, path2data=path2data, plotting=False, vstep=361))
+    except:
+        print("error at ", lat)
 
-# for result in range(len(lats)):
-#     cur_lat = lats[result]
-#     result_v = ray.get(obj_list[result])
-#     print("Latitude",cur_lat, "velocity ", result_v)
-#     v_corr[np.where(cur_lat == np.around(latitude,2))] = result_v 
+for result in range(len(lats)):
+    cur_lat = lats[result]
+    result_v = ray.get(obj_list[result])
+    print("Latitude",cur_lat, "velocity ", result_v)
+    v_corr[np.where(cur_lat == np.around(latitude,2))] = result_v 
 
 
 
-# end = time()
-# print("Program Runtime ", end - start)
-# #Plot results along with currently accepted ZWP to compare. 
-# path2wp = path2data + 'ZWP_j2016_PJ03.txt'
-# lat_zwp, zwp = readZWP(path2wp) 
-# fig, axs = plt.subplots(1, 1,figsize=(8,4))
-# axs.plot(zwp,lat_zwp,label='JT - ZWP')
-# axs.plot(v_corr,latitude,label='DP')
-# axs.set_ylabel('Latitude (deg)')
-# axs.set_xlabel('Velocity (m/s)')
-# axs.set_ylim([-60,60])
-# plt.show()
-# ray.shutdown()
+end = time()
+print("Program Runtime ", end - start)
+#Plot results along with currently accepted ZWP to compare. 
+path2wp = path2data + 'ZWP_j2016_PJ03.txt'
+lat_zwp, zwp = readZWP(path2wp) 
+fig, axs = plt.subplots(1, 1,figsize=(8,4))
+axs.plot(zwp,lat_zwp,label='JT - ZWP')
+axs.plot(v_corr,latitude,label='DP')
+axs.set_ylabel('Latitude (deg)')
+axs.set_xlabel('Velocity (m/s)')
+axs.set_ylim([-60,60])
+plt.show()
+ray.shutdown()
 
