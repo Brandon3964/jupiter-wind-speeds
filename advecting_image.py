34d2c86844260647d09b2becefd8ab97e7a14c1e
Brandon <brrrandonhuang@gmail.com>
2022-01-29 00:04:25 -0800

    try full img

--- advecting_image.py
+++ advecting_image.py
@@ -13,68 +13,68 @@ images = glob.glob(path2advected + '*.fits')
 # copied_image = glob.glob(path2advected + '*.fits')
 # copied_image.remove('./advected_data/190409_631_1639_reg_trim.fits')
 advected_target = './new_201904/190409_631_1639_reg_trim.fits'
-# for file in images:
+for file in images:
 
 
-#     hdul = fits.open(file)
-#     temp = hdul[0].data
-#     result_lon = []
+    hdul = fits.open(file)
+    temp = hdul[0].data
+    result_lon = []
 
 
 
-#     file1 = open('./full_201904_result.txt', 'r')
-#     count = 0
-# # for other_img in copied_image:
-#     for line in file1:
-#         count += 1
-#         temp2 = (line.strip()).split()
-#         y = geographic2pixel(advected_target, 0, float(temp2[0]))
-#         result_lon.append(advection(file, advected_target, y[1], float(temp2[1]))[0])
+    file1 = open('./full_201904_result.txt', 'r')
+    count = 0
+# for other_img in copied_image:
+    for line in file1:
+        count += 1
+        temp2 = (line.strip()).split()
+        y = geographic2pixel(advected_target, 0, float(temp2[0]))
+        result_lon.append(advection(file, advected_target, y[1], float(temp2[1]))[0])
 
-#     advected_rows = []
-#     full_deg = []
-#     start = 0
-#     while start < 360:
-#         full_deg.append(round(start, 1))
-#         start += 0.05
+    advected_rows = []
+    full_deg = []
+    start = 0
+    while start < 360:
+        full_deg.append(round(start, 1))
+        start += 0.05
 
-#     for item in range(0, len(result_lon)):
-#         advected_rows.append(np.interp(full_deg, result_lon[item], temp[item], left = np.nan, right = np.nan))
-#     hdul[0].data = advected_rows
-#     hdul.writeto(file,overwrite=True)
-#     file1.close()
-#     hdul.close()
-# print("done")
-
-test_target = './advected_data/190409_631_0833_reg_trim.fits'
-
-result_lon = []
-
-file1 = open('./full_201904_result.txt', 'r')
-count = 0
-
-for line in file1:
-    count += 1
-    temp2 = (line.strip()).split()
-    y = geographic2pixel(test_target, 0, float(temp2[0]))
-    result_lon.append(advection(test_target, advected_target, y[1], float(temp2[1]))[0])
-
-advected_rows = []
-full_deg = []
-start = 0
-while start < 360:
-    full_deg.append(round(start, 1))
-    start += 0.05
-
-
-hdul = fits.open(test_target)
-temp = hdul[0].data
-for item in range(0, len(result_lon)):
-    advected_rows.append(np.interp(full_deg, result_lon[item], temp[item], left = np.nan, right = np.nan))
-file1.close()
-hdul[0].data = advected_rows
-hdul.writeto(test_target,overwrite=True)
-
-hdul.close()
+    for item in range(0, len(result_lon)):
+        advected_rows.append(np.interp(full_deg, result_lon[item], temp[item], left = np.nan, right = np.nan))
+    hdul[0].data = advected_rows
+    hdul.writeto(file,overwrite=True)
+    file1.close()
+    hdul.close()
 print("done")
+
+# test_target = './advected_data/190409_631_0833_reg_trim.fits'
+
+# result_lon = []
+
+# file1 = open('./full_201904_result.txt', 'r')
+# count = 0
+
+# for line in file1:
+#     count += 1
+#     temp2 = (line.strip()).split()
+#     y = geographic2pixel(test_target, 0, float(temp2[0]))
+#     result_lon.append(advection(test_target, advected_target, y[1], float(temp2[1]))[0])
+
+# advected_rows = []
+# full_deg = []
+# start = 0
+# while start < 360:
+#     full_deg.append(round(start, 1))
+#     start += 0.05
+
+
+# hdul = fits.open(test_target)
+# temp = hdul[0].data
+# for item in range(0, len(result_lon)):
+#     advected_rows.append(np.interp(full_deg, result_lon[item], temp[item], left = np.nan, right = np.nan))
+# file1.close()
+# hdul[0].data = advected_rows
+# hdul.writeto(test_target,overwrite=True)
+
+# hdul.close()
+
 
